f74ffee0fb4cde9ddb519f306df362b688f14bea
Josh Klontz <Joshua.Klontz@noblis.org>
2013-10-03 13:30:06 -0400

    cleaned up SDK_PATH specification

--- src/tests/attribute.cpp
+++ src/tests/attribute.cpp
@@ -5,16 +5,12 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc > 2) {
-        printf("Usage: attribute [sdk_path]");
+    if (argc != 2) {
+        printf("Usage: attribute sdk_path");
         return 1;
     }
 
-    const char *sdk_path;
-    if (argc > 1) sdk_path = argv[1];
-    else          sdk_path = "../";
-
-    janus_error error = janus_initialize(sdk_path);
+    janus_error error = janus_initialize(argv[1]);
     if (error != JANUS_SUCCESS) {
         printf("Failed with error: %d\n", error);
         abort();
