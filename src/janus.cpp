7c1649d4cf278c7f54b4657478edc4cfedf19418
Josh Klontz <Joshua.Klontz@noblis.org>
2014-02-18 16:48:20 -0500

    redesigned ENUM_CASE

--- src/janus.cpp
+++ src/janus.cpp
@@ -7,25 +7,25 @@ size_t janus_max_template_size()
     return JANUS_MAX_TEMPLATE_SIZE_LIMIT;
 }
 
-#define ENUM_CASE(X) case X: return #X;
+#define ENUM_CASE(X) case JANUS_##X: return #X;
 #define ENUM_DEFAULT default: return "???";
 
 const char *janus_error_to_string(janus_error error)
 {
     switch (error) {
-        ENUM_CASE(JANUS_SUCCESS)
-        ENUM_CASE(JANUS_UNKNOWN_ERROR)
-        ENUM_CASE(JANUS_OUT_OF_MEMORY)
-        ENUM_CASE(JANUS_INVALID_SDK_PATH)
-        ENUM_CASE(JANUS_OPEN_ERROR)
-        ENUM_CASE(JANUS_READ_ERROR)
-        ENUM_CASE(JANUS_WRITE_ERROR)
-        ENUM_CASE(JANUS_PARSE_ERROR)
-        ENUM_CASE(JANUS_INVALID_IMAGE)
-        ENUM_CASE(JANUS_INVALID_VIDEO)
-        ENUM_CASE(JANUS_MISSING_TEMPLATE_ID)
-        ENUM_CASE(JANUS_MISSING_FILE_NAME)
-        ENUM_CASE(JANUS_NULL_ATTRIBUTE_LIST)
+        ENUM_CASE(SUCCESS)
+        ENUM_CASE(UNKNOWN_ERROR)
+        ENUM_CASE(OUT_OF_MEMORY)
+        ENUM_CASE(INVALID_SDK_PATH)
+        ENUM_CASE(OPEN_ERROR)
+        ENUM_CASE(READ_ERROR)
+        ENUM_CASE(WRITE_ERROR)
+        ENUM_CASE(PARSE_ERROR)
+        ENUM_CASE(INVALID_IMAGE)
+        ENUM_CASE(INVALID_VIDEO)
+        ENUM_CASE(MISSING_TEMPLATE_ID)
+        ENUM_CASE(MISSING_FILE_NAME)
+        ENUM_CASE(NULL_ATTRIBUTE_LIST)
         ENUM_DEFAULT
     }
 }
@@ -33,14 +33,14 @@ const char *janus_error_to_string(janus_error error)
 const char *janus_attribute_to_string(janus_attribute attribute)
 {
     switch (attribute) {
-        ENUM_CASE(JANUS_INVALID_ATTRIBUTE)
-        ENUM_CASE(JANUS_FRAME)
-        ENUM_CASE(JANUS_RIGHT_EYE_X)
-        ENUM_CASE(JANUS_RIGHT_EYE_Y)
-        ENUM_CASE(JANUS_LEFT_EYE_X)
-        ENUM_CASE(JANUS_LEFT_EYE_Y)
-        ENUM_CASE(JANUS_NOSE_BASE_X)
-        ENUM_CASE(JANUS_NOSE_BASE_Y)
+        ENUM_CASE(INVALID_ATTRIBUTE)
+        ENUM_CASE(FRAME)
+        ENUM_CASE(RIGHT_EYE_X)
+        ENUM_CASE(RIGHT_EYE_Y)
+        ENUM_CASE(LEFT_EYE_X)
+        ENUM_CASE(LEFT_EYE_Y)
+        ENUM_CASE(NOSE_BASE_X)
+        ENUM_CASE(NOSE_BASE_Y)
         ENUM_DEFAULT
     }
 }
