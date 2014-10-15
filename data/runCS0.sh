985a1a570b57c61cedb83154655056532ac3f331
bhklein <benhunter1990@gmail.com>
2014-10-15 14:40:40 -0400

    Loop over protocols

--- data/runCS0.sh
+++ data/runCS0.sh
@@ -30,50 +30,30 @@ ALGORITHM=""
 for i in `seq 1 10`;
 do
 	mkdir ${results}/split${i}
-	# Protocol A
-	# Enroll galleries using "janus_create_templates" for use in "janus_evaluate_verify"
-	janus_create_templates $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_A_probe.csv $GALLERY/test_${i}_A_probe_templates.gal $ALGORITHM
-	janus_create_templates $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_A_gal.csv $GALLERY/test_${i}_A_gal_templates.gal $ALGORITHM
+	for PROTOCOL in A B
+	do
+		echo $PROTOCOL
+		# Enroll galleries using "janus_create_templates" for use in "janus_evaluate_verify"
+		janus_create_templates $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_${PROTOCOL}_probe.csv $GALLERY/test_${i}_${PROTOCOL}_probe_templates.gal $ALGORITHM
+		janus_create_templates $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_${PROTOCOL}_gal.csv $GALLERY/test_${i}_${PROTOCOL}_gal_templates.gal $ALGORITHM
 
-	# Evaluate protocol A verification using "janus_evaluate_verify"
-	janus_evaluate_verify $SDK_PATH $TEMP_PATH $GALLERY/test_${i}_A_gal_templates.gal $GALLERY/test_${i}_A_probe_templates.gal $CS0_DIR/protocol/split${i}/test_${i}_A_gal.csv $CS0_DIR/protocol/split${i}/test_${i}_A_probe.csv $MATRIX/verify_${i}_A.mtx $MATRIX/verify_${i}_A.mask $ALGORITHM
+		# Evaluate protocol A verification using "janus_evaluate_verify"
+		janus_evaluate_verify $SDK_PATH $TEMP_PATH $GALLERY/test_${i}_${PROTOCOL}_gal_templates.gal $GALLERY/test_${i}_${PROTOCOL}_probe_templates.gal $CS0_DIR/protocol/split${i}/test_${i}_${PROTOCOL}_gal.csv $CS0_DIR/protocol/split${i}/test_${i}_${PROTOCOL}_probe.csv $MATRIX/verify_${i}_${PROTOCOL}.mtx $MATRIX/verify_${i}_${PROTOCOL}.mask $ALGORITHM
 
-	# Enroll target gallery using "janus_create_gallery" for use in "janus_evaluate_search"
-	janus_create_gallery $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_A_gal.csv $GALLERY/test_${i}_A_gal_flat.gal $ALGORITHM
+		# Enroll target gallery using "janus_create_gallery" for use in "janus_evaluate_search"
+		janus_create_gallery $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_${PROTOCOL}_gal.csv $GALLERY/test_${i}_${PROTOCOL}_gal_flat.gal $ALGORITHM
 
-	#Evaluate protocol A search using "janus_evaluate_search"
-	janus_evaluate_search $SDK_PATH $TEMP_PATH $GALLERY/test_${i}_A_gal_flat.gal $GALLERY/test_${i}_A_probe_templates.gal $CS0_DIR/protocol/split${i}/test_${i}_A_gal.csv $CS0_DIR/protocol/split${i}/test_${i}_A_probe.csv $MATRIX/search_${i}_A.mtx $MATRIX/search_${i}_A.mask 150 $ALGORITHM
+		#Evaluate protocol A search using "janus_evaluate_search"
+		janus_evaluate_search $SDK_PATH $TEMP_PATH $GALLERY/test_${i}_${PROTOCOL}_gal_flat.gal $GALLERY/test_${i}_${PROTOCOL}_probe_templates.gal $CS0_DIR/protocol/split${i}/test_${i}_${PROTOCOL}_gal.csv $CS0_DIR/protocol/split${i}/test_${i}_${PROTOCOL}_probe.csv $MATRIX/search_${i}_${PROTOCOL}.mtx $MATRIX/search_${i}_${PROTOCOL}.mask 150 $ALGORITHM
 
-	# Protocol B
-	# Enroll galleries using "janus_create_templates" for use in "janus_evaluate_verify"
-	janus_create_templates $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_B_probe.csv $GALLERY/test_${i}_B_probe_templates.gal $ALGORITHM
-	janus_create_templates $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_B_gal.csv $GALLERY/test_${i}_B_gal_templates.gal $ALGORITHM
-
-	# Evaluate protocol B verification using "janus_evaluate_verify"
-	janus_evaluate_verify $SDK_PATH $TEMP_PATH $GALLERY/test_${i}_B_gal_templates.gal $GALLERY/test_${i}_B_probe_templates.gal $CS0_DIR/protocol/split${i}/test_${i}_B_gal.csv $CS0_DIR/protocol/split${i}/test_${i}_B_probe.csv $MATRIX/verify_${i}_B.mtx $MATRIX/verify_${i}_B.mask $ALGORITHM
-
-	# Enroll target gallery using "janus_create_gallery" for use in "janus_evaluate_search"
-	janus_create_gallery $SDK_PATH $TEMP_PATH $CS0_DIR $CS0_DIR/protocol/split${i}/test_${i}_B_gal.csv $GALLERY/test_${i}_B_gal_flat.gal $ALGORITHM
-
-	# Evaluate protocol B search using "janus_evaluate_search"
-	janus_evaluate_search $SDK_PATH $TEMP_PATH $GALLERY/test_${i}_B_gal_flat.gal $GALLERY/test_${i}_B_probe_templates.gal $CS0_DIR/protocol/split${i}/test_${i}_B_gal.csv $CS0_DIR/protocol/split${i}/test_${i}_B_probe.csv $MATRIX/search_${i}_B.mtx $MATRIX/search_${i}_B.mask 150 $ALGORITHM
-
-	# Evaluate output matrices using OpenBR
-	# Protocol A
-	br -eval $MATRIX/verify_${i}_A.mtx $MATRIX/verify_${i}_A.mask $RESULTS/split${i}/verify_${i}_A.csv -plot $RESULTS/split${i}/verify_${i}_A.csv $RESULTS/split${i}/verify_${i}_A.pdf
-	br -eval $MATRIX/search_${i}_A.mtx $MATRIX/search_${i}_A.mask $RESULTS/split${i}/search_${i}_A.csv -plot $RESULTS/split${i}/search_${i}_A.csv $RESULTS/split${i}/search_${i}_A.pdf
-	# Take ROC/DET curves from verify and CMC curve from search
-	pdftk A=$RESULTS/split${i}/verify_${i}_A.pdf B=$RESULTS/split${i}/search_${i}_A.pdf cat A1-5 B6 A7-8 output $RESULTS/split${i}/split${i}_A_results.pdf
-	rm $RESULTS/split${i}/verify_${i}_A.pdf
-	rm $RESULTS/split${i}/search_${i}_A.pdf
-
-	# Protocol B
-	br -eval $MATRIX/verify_${i}_B.mtx $MATRIX/verify_${i}_B.mask $RESULTS/split${i}/verify_${i}_B.csv -plot $RESULTS/split${i}/verify_${i}_B.csv $RESULTS/split${i}/verify_${i}_B.pdf
-	br -eval $MATRIX/search_${i}_B.mtx $MATRIX/search_${i}_B.mask $RESULTS/split${i}/search_${i}_B.csv -plot $RESULTS/split${i}/search_${i}_B.csv $RESULTS/split${i}/search_${i}_B.pdf
-	# Take ROC/DET curves from verify and CMC curve from search
-	pdftk A=$RESULTS/split${i}/verify_${i}_B.pdf B=$RESULTS/split${i}/search_${i}_B.pdf cat A1-5 B6 A7-8 output $RESULTS/split${i}/split${i}_B_results.pdf
-	rm $RESULTS/split${i}/verify_${i}_B.pdf
-	rm $RESULTS/split${i}/search_${i}_B.pdf
+		# Evaluate output matrices using OpenBR
+		br -eval $MATRIX/verify_${i}_${PROTOCOL}.mtx $MATRIX/verify_${i}_${PROTOCOL}.mask $RESULTS/split${i}/verify_${i}_${PROTOCOL}.csv -plot $RESULTS/split${i}/verify_${i}_${PROTOCOL}.csv $RESULTS/split${i}/verify_${i}_${PROTOCOL}.pdf
+		br -eval $MATRIX/search_${i}_${PROTOCOL}.mtx $MATRIX/search_${i}_${PROTOCOL}.mask $RESULTS/split${i}/search_${i}_${PROTOCOL}.csv -plot $RESULTS/split${i}/search_${i}_${PROTOCOL}.csv $RESULTS/split${i}/search_${i}_${PROTOCOL}.pdf
+		# Take ROC/DET curves from verify and CMC curve from search
+		pdftk A=$RESULTS/split${i}/verify_${i}_${PROTOCOL}.pdf B=$RESULTS/split${i}/search_${i}_${PROTOCOL}.pdf cat A1-5 B6 A7-8 output $RESULTS/split${i}/split${i}_${PROTOCOL}_results.pdf
+		rm $RESULTS/split${i}/verify_${i}_${PROTOCOL}.pdf
+		rm $RESULTS/split${i}/search_${i}_${PROTOCOL}.pdf
+	done
 	rm $RESULTS/split${i}/*.R
 done
 
