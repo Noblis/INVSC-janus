8753133e6b323ce74f128cefdeebb4b39f34c195
bhklein <benhunter1990@gmail.com>
2015-06-05 16:09:30 -0400

    update janus_evaluate_search and janus_create_gallery to operate on opaque galleries

--- src/utils/janus_evaluate_search.cpp
+++ src/utils/janus_evaluate_search.cpp
@@ -25,23 +25,23 @@ int main(int argc, char *argv[])
         printUsage();
         return 1;
     }
-    const char *ext1 = get_ext(argv[3]);
-    const char *ext2 = get_ext(argv[4]);
-    const char *ext3 = get_ext(argv[6]);
-    const char *ext4 = get_ext(argv[6]);
-    const char *ext5 = get_ext(argv[7]);
-    const char *ext6 = get_ext(argv[8]);
 
-    if (strcmp(ext1, "gal") != 0 || strcmp(ext2, "gal") != 0) {
-        printf("Gallery files must be \".gal\" format.\n");
+    const char *ext1 = get_ext(argv[4]);
+    const char *ext2 = get_ext(argv[5]);
+    const char *ext3 = get_ext(argv[6]);
+    const char *ext4 = get_ext(argv[7]);
+    const char *ext5 = get_ext(argv[8]);
+
+    if (strcmp(ext1, "gal") != 0) {
+        printf("Query gallery file must be \".gal\" format.\n");
         return 1;
-    } else if (strcmp(ext3, "csv") != 0 || strcmp(ext4, "csv") != 0) {
+    } else if (strcmp(ext2, "csv") != 0 || strcmp(ext3, "csv") != 0) {
         printf("Metadata files must be \".csv\" format. \n");
         return 1;
-    } else if (strcmp(ext5, "mtx") != 0) {
+    } else if (strcmp(ext4, "mtx") != 0) {
         printf("Similarity matrix files should be \".mtx\" format.");
         return 1;
-    } else if (strcmp(ext6, "mask") != 0) {
+    } else if (strcmp(ext5, "mask") != 0) {
         printf("Mask matrix files should be \".mask\" format.");
         return 1;
     }
@@ -58,15 +58,7 @@ int main(int argc, char *argv[])
     JANUS_ASSERT(janus_initialize(argv[1], argv[2], algorithm))
     size_t num_requested_returns = atoi(argv[9]);
 
-    std::ifstream target;
-    target.open(argv[3], std::ios::binary | std::ios::ate);
-    size_t target_bytes = target.tellg();
-    target.seekg(0, ios::beg);
-
-    janus_flat_gallery target_flat = new janus_data[target_bytes];
-    target.read((char*)target_flat, target_bytes);
-
-    JANUS_ASSERT(janus_evaluate_search(target_flat, target_bytes, argv[4], argv[5], argv[6], argv[7], argv[8], num_requested_returns))
+    JANUS_ASSERT(janus_evaluate_search(argv[3], argv[4], argv[5], argv[6], argv[7], argv[8], num_requested_returns))
     JANUS_ASSERT(janus_finalize())
 
     janus_print_metrics(janus_get_metrics());
