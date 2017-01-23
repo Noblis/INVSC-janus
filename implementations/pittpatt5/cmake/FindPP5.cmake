f767cf9d8482463e30739a1a0c6e156a00ed0b62
Jordan Cheney <jcheney@mars.bio.noblis.org>
2017-01-23 15:36:23 -0500

    Stop using link_directories

--- implementations/pittpatt5/cmake/FindPP5.cmake
+++ implementations/pittpatt5/cmake/FindPP5.cmake
@@ -8,15 +8,36 @@
 #   target_link_libraries(MY_TARGET ${PP5_LIBS})       #
 # ==================================================== #
 
+macro(find_pittpatt_library LIB_VAR LIB_NAME)
+  find_library(${LIB_VAR} NAMES ${LIB_NAME}
+                          PATHS ${PP5_DIR}/lib)
+  
+  if (NOT ${LIB_VAR})
+    message(WARNING "Could not find PittPatt library: ${LIB_NAME} in ${PP5_DIR}/lib")
+    set(PP5_FOUND FALSE)
+  endif()
+
+  mark_as_advanced(${LIB_VAR})
+endmacro()
+
+# Look for the PittPatt5 directory
 include(FindPackageHandleStandardArgs)
 find_path(PP5_DIR include/pittpatt_sdk.h)
 find_package_handle_standard_args(PP5 DEFAULT_MSG PP5_DIR)
 set(PP5_FOUND ${PP5_FOUND})
+
+# If we found a PittPatt5 directory try and find all the relevant libraries
+set(PP5_LIBS)
 if(PP5_FOUND)
   include_directories(${PP5_DIR}/include)
-  link_directories(${PP5_DIR}/lib)
-  set(PP5_LIBS pittpatt_raw_image
-               pittpatt_raw_image_io
-               pittpatt_sdk
-               pittpatt_video_io)
+
+  find_pittpatt_library(PP5_RAW_IMAGE    pittpatt_raw_image)
+  find_pittpatt_library(PP5_RAW_IMAGE_IO pittpatt_raw_image_io)
+  find_pittpatt_library(PP5_SDK          pittpatt_sdk)
+  find_pittpatt_library(PP5_VIDEO_IO     pittpatt_video_io)
+
+  set(PP5_LIBS ${PP5_RAW_IMAGE}
+               ${PP5_RAW_IMAGE_IO}
+               ${PP5_SDK}
+               ${PP5_VIDEO_IO})        
 endif()
