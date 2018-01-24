b412b602ad121b2195820424821f16d14fb31772
Mireles, Nicholas <Nick.Mireles@noblis.org>
2018-01-24 17:58:24 -0500

    Revert "Fixed the configuration reference"
    This reverts commit c6af907cc82a5afd455592f5f34d9ff9c0de559e.

--- src/cognitec/cognitec.cpp
+++ src/cognitec/cognitec.cpp
@@ -18,11 +18,11 @@
 
 using namespace std;
 
-FRsdk::Configuration::Configuration config;
+FRsdk::Configuration config;
 
 janus_error janus_initialize(const string &sdk_path, const string &, const string &algorithm, const int)
 {
-	config = new FRsdk::Configuration::Configuration(algorithm);
+	config = new FRsdk::Configuration(algorithm);
 
 }
 janus_error janus_detect(const janus_media &media, const size_t min_face_size, std::vector<janus_track> &tracks)
