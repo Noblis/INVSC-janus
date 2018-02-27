0c5ee8ea1b440fdde51b6872bff72d5342d5b7e8
Mireles, Nicholas <Nick.Mireles@noblis.org>
2018-02-27 16:52:06 -0500

    Added in image implementation, fixed some more problems

--- src/cognitec/cognitec.cpp
+++ src/cognitec/cognitec.cpp
@@ -17,6 +17,7 @@
 #include <FRsdk/face.h>
 #include <FRsdk/portrait.h>
 #include <FRsdk/eyes.h>
+#include <FRsdk/image.h>
 
 using namespace std;
 using namespace FRsdk;
@@ -42,7 +43,7 @@ public:
 	  }
   }
 
-  bool isColor() {return c;}
+  bool isColor() const {return c;}
 
   unsigned int width() const {return w;}
 
@@ -79,12 +80,12 @@ Face::Finder *face_detector;
 Eyes::Finder *eye_detector;
 Portrait::Analyzer *portrait_analyzer;
 
-void to_cognitec_image(janus_media media, vector<CountedPtr<JanusBody>> &image_bodies)
+void to_cognitec_image(janus_media media, vector<Image> &cognitec_images)
 {
 	for (size_t i = 0; i< media.data.size();i++)
 	{
 		JanusBody body = new JanusBody(media.data[0],media.width,media.height,media.step,media.color_space);
-		image_bodies.push_back(CountedPtr<JanusBody>(&body));
+		cognitec_images.push_back(Image(CountedPtr<JanusBody>(&body)));
 	}
 }
 
@@ -100,26 +101,26 @@ janus_error janus_initialize(const string &sdk_path, const string &, const strin
 }
 janus_error janus_detect(const janus_media &media, const size_t min_face_size, std::vector<janus_track> &tracks)
 {
-	vector<FRsdk::CountedPtr<JanusBody>> image_bodies;
-	to_cognitec_image(media,image_bodies);
+	vector<Image> cognitec_images;
+	to_cognitec_image(media,cognitec_images);
 
-	for (size_t i = 0; i< image_bodies.size();i++)
+	for (size_t i = 0; i< cognitec_images.size();i++)
 	{
 		//Find interpupillary markers in the image
-		Face::LocationSet locations = face_detector->find(image_bodies[i]);
+		Face::LocationSet locations = face_detector->find(cognitec_images[i]);
         Face::LocationSet::const_iterator faceIter = locations.begin();
 
         vector<pair<float,FRsdk::Portrait::Characteristics> > face_confidences;
         while( faceIter != locations.end())
         {
         		// Detect the eyes in the face locations
-        		Eyes::LocationSet eye_locations = eye_detector->find(image_bodies[i],*faceIter);
+        		Eyes::LocationSet eye_locations = eye_detector->find(cognitec_images[i],*faceIter);
 
         		//Choose the eyes with the highest confidence
         		Eyes::Location &eye_location = eye_locations.front();
 
         		//Create the image annotated with the eye information
-			AnnotatedImage annotated (image_bodies[i],eye_location);
+			AnnotatedImage annotated (cognitec_images[i],eye_location);
 
 			//Get the face characteristics from the annotated image
 			FRsdk::Portrait::Characteristics face_metadata = portrait_analyzer->analyze(annotated);
@@ -135,12 +136,12 @@ janus_error janus_detect(const janus_media &media, const size_t min_face_size, s
         		janus_track track;
         		janus_attributes attributes;
 
-        		attributes.face_height = face_confidences[j].second.height();
         		attributes.face_width = face_confidences[j].second.width();
 
 			if (attributes.face_width < (int)min_face_size)
 				continue;
 
+			attributes.face_height = face_confidences[j].second.height();
         		attributes.face_x = face_confidences[j].second.faceCenter().x() - (attributes.face_width/2);
         		attributes.face_y = face_confidences[j].second.faceCenter().y() - (attributes.face_height/2);
 
