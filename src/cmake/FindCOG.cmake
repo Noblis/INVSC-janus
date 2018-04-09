823d3c8d0474d3b057851abeee1dc9bc513f03b5
Nick Mireles <nmireles@artemis.bio.noblis.org>
2018-04-09 12:11:30 -0400

    Changed the cmake script back to standard form. Change the name of the libraries instead.

--- src/cmake/FindCOG.cmake
+++ src/cmake/FindCOG.cmake
@@ -13,11 +13,18 @@ find_path(COG_DIR include/FRsdk/config.h)
 find_package_handle_standard_args(COG DEFAULT_MSG COG_DIR)
 set(COG_FOUND ${COG_FOUND})
 if(COG_FOUND)
+  set(CMAKE_STATIC_LIBRARY_PREFIX "lib")
+  set(VERSION "")
   include_directories(${COG_DIR}/include)
-  link_directories(${cog_DIR}/lib/x86_64/gcc-4.8-sse2)
+  #This is because the folder structure only contains major.minor and this variable contains the patch version too.
+  string(SUBSTRING ${CMAKE_CXX_COMPILER_VERSION} 0 3 VERSION)
+  set(COG_LIB_DIR ${COG_DIR}/lib/x86_64/gcc-${VERSION}-sse2/)
+  message(STATUS ${COG_LIB_DIR})
+  link_directories(${COG_LIB_DIR})
   set(COG_LIBS biospi
                jfrsdkjni
                output
                frsdk
                simio)
+  message(STATUS ${COG_LIBS})
 endif()
